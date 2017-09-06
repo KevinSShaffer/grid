adf116588ecd2e3b43a1a09c44e0d6425bf0ddb8
Kevin Shaffer <kevinsshaffer@gmail.com>
2017-09-06 00:50:17 -0600

    add alternate constructor with seed to grid class

--- grid.cpp
+++ grid.cpp
@@ -2,11 +2,20 @@
 #include "grid.h"
 #include <vector>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 Grid::Grid(int rows, int cols) :
 	_vec(rows, std::vector<int>(cols))
 {
 	// seed a RNG with current date time
+	srand(time(0));
+}
+Grid::Grid(int rows, int cols, int seed) :
+	_vec(rows, std::vector<int>(cols))
+{
+	srand(seed);	
 }
 
 std::string Grid::ToString()
@@ -17,14 +26,15 @@ std::string Grid::ToString()
 		it_x != _vec.end();
 		++it_x)
 	{
+		if (it_x != _vec.begin())
+			output.append("\n");
+
 		for (std::vector<int>::iterator it_y = it_x->begin();
 			it_y != it_x->end();
 			++it_y)
 		{
 			output += *it_y + 48;
 		}
-
-		output.append("\n");
 	}
 
 	return output;
--- grid.h
+++ grid.h
@@ -8,6 +8,6 @@ private:
 	std::vector< std::vector<int> > _vec;
 public:
 	Grid(int rows, int cols);
-	// add constructor with seed Grid(int rows, int cols, int seed);
+	Grid(int rows, int cols, int seed);
 	std::string ToString();
 };
